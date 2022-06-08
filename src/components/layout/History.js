d8ab519feefd47649d10ac35c114ce7e007c3f7e
Mario Delgado <mdelgado1138@hotmail.com>
2022-06-08 16:38:40 -0700

    more media queries

--- src/components/layout/History.js
+++ src/components/layout/History.js
@@ -6,42 +6,44 @@ import ImageGrid from './ImageGrid';
 const History = () => {
   return (
     <React.Fragment>
-      <div className="container-text container-text-50">
-        <ImageGrid />
-      </div>
-      <div className="container-text container-text-50 flex-column">
-        <h2>Our History</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cumque
-          voluptatum est quam dolores vel, sit accusamus id harum facilis
-          eveniet nulla quis asperiores eius minus inventore, doloribus
-          necessitatibus fugit.
-        </p>
-        <Route path="/about">
+      <div className="flex-wrapper">
+        <div className="container-img container-50">
+          <ImageGrid />
+        </div>
+        <div className="container-text container-50 flex-column">
+          <h2>Our History</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cumque
             voluptatum est quam dolores vel, sit accusamus id harum facilis
             eveniet nulla quis asperiores eius minus inventore, doloribus
-            necessitatibus fugit. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Qui cumque voluptatum est quam dolores vel, sit
-            accusamus id harum facilis eveniet nulla quis asperiores eius minus
-            inventore, doloribus necessitatibus fugit.
+            necessitatibus fugit.
           </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui cumque
-            voluptatum est quam dolores vel, sit accusamus id harum facilis
-            eveniet nulla quis asperiores eius minus inventore, doloribus
-            necessitatibus fugit. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Qui cumque voluptatum est quam dolores vel, sit
-            accusamus id harum facilis eveniet nulla quis asperiores eius minus
-            inventore, doloribus necessitatibus fugit.
-          </p>
-        </Route>
-        <Route path="/home">
-          <Link to="/about">
-            <button className="btn">Read More</button>
-          </Link>
-        </Route>
+          <Route path="/about">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              cumque voluptatum est quam dolores vel, sit accusamus id harum
+              facilis eveniet nulla quis asperiores eius minus inventore,
+              doloribus necessitatibus fugit. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Qui cumque voluptatum est quam
+              dolores vel, sit accusamus id harum facilis eveniet nulla quis
+              asperiores eius minus inventore, doloribus necessitatibus fugit.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              cumque voluptatum est quam dolores vel, sit accusamus id harum
+              facilis eveniet nulla quis asperiores eius minus inventore,
+              doloribus necessitatibus fugit. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Qui cumque voluptatum est quam
+              dolores vel, sit accusamus id harum facilis eveniet nulla quis
+              asperiores eius minus inventore, doloribus necessitatibus fugit.
+            </p>
+          </Route>
+          <Route path="/home">
+            <Link to="/about">
+              <button className="btn">Read More</button>
+            </Link>
+          </Route>
+        </div>
       </div>
     </React.Fragment>
   );
